c4fae3204b2f1487c761439d77cdf60681dc1bf0
Adithyakondoju <163967257+Adithyakondoju@users.noreply.github.com>
2024-12-03 23:01:12 +0530

    Delete my.py

--- my.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import pickle as pk
-
-model = pk.load(open('model.pkl','rb'))
-scaler = pk.load(open('scaler.pkl','rb'))
-
-st.header('Loan Predcition App')
